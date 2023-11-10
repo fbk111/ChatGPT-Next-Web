6ad7d859e15fb08d00a3cfa25326ec6e1edd9ea4
F csummer <2290291854@qq.com>
2023-11-10 06:41:33 +0000

    修改配置文件

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,11 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>
+          {process.env.APP_NAME_SELF}
+        </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          {process.env.APP_DESC_SELF}
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
